7a190a56de5a6351aa4f50420a3339937291dbd4
sowmi-source <sowmiyag62000@gmail.com>
2025-09-29 15:38:23 +0530

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,6 +1,7 @@
-INSERT INTO packages (accomodation, activites, food, packages_name, place_name, total_price, transportation)
+INSERT INTO packages (id,accomodation, activites, food, packages_name, place_name, total_price, transportation)
 VALUES
 (1,2000, 'Sightseeing, Temple Visit', 1500, 'Spiritual Package', 'Varanasi', 12000, 3000),
 (2,3500, 'Trekking, Camping', 2500, 'Adventure Package', 'Manali', 25000, 8000),
 (3,1800, 'Beach Walk, Water Sports', 2000, 'Beach Holiday', 'Goa', 18000, 5000),
 (4,4000, 'Wild Safari, Bird Watching', 3000, 'Wildlife Tour', 'Jim Corbett', 22000, 7000);
+
