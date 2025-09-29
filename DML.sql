144116afee781b0be0794c10497afe5629cadbca
sowmi-source <sowmiyag62000@gmail.com>
2025-09-29 15:24:11 +0530

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,8 +1,9 @@
-INSERT INTO packages (accomodation, activities, food, packages_name, place_name, total_price, transportation)
+INSERT INTO packages (id,accomodation, activities, food, packages_name, place_name, total_price, transportation)
 VALUES
-('Hotel Deluxe', 'Sightseeing, Hiking', 'Breakfast, Dinner', 'Adventure Trip', 'Ooty', 15000.00, 'Bus'),
-('Resort Stay', 'Boating, Trekking', 'All Meals', 'Nature Escape', 'Kodaikanal', 18000.00, 'Car'),
-('Cottage', 'Safari, Photography', 'Breakfast', 'Wildlife Tour', 'Mudumalai', 12000.00, 'Jeep'),
-('Guest House', 'Temple Visit, City Tour', 'Breakfast, Lunch', 'Cultural Trip', 'Madurai', 10000.00, 'Bus');
+(1,'Hotel Deluxe', 'Sightseeing, Hiking', 'Breakfast, Dinner', 'Adventure Trip', 'Ooty', 15000.00, 'Bus'),
+(2,'Resort Stay', 'Boating, Trekking', 'All Meals', 'Nature Escape', 'Kodaikanal', 18000.00, 'Car'),
+(3,'Cottage', 'Safari, Photography', 'Breakfast', 'Wildlife Tour', 'Mudumalai', 12000.00, 'Jeep'),
+(4,'Guest House', 'Temple Visit, City Tour', 'Breakfast, Lunch', 'Cultural Trip', 'Madurai', 10000.00, 'Bus');
+
 
 
