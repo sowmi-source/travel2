8325bd378941f570f61f76613e36ffe004003e5b
sowmi-source <sowmiyag62000@gmail.com>
2025-09-29 13:40:13 +0530

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,11 +1,11 @@
 
-CREATE TABLE bank_customer (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    phone_number VARCHAR(15) NOT NULL UNIQUE,
-    account_type VARCHAR(20) NOT NULL,
-    balance DECIMAL(15,2) NOT NULL
+CREATE TABLE packages (
+    id INT PRIMARY KEY,
+    accomodation VARCHAR(255) NOT NULL,
+    activities VARCHAR(500),
+    food VARCHAR(255),
+    packages_name VARCHAR(255) NOT NULL,
+    place_name VARCHAR(255) NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    transportation VARCHAR(255)
 );
-
-
